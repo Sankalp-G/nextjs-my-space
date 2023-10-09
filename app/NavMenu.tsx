e3031ce357d4b0654822561aa60b328e6f454052
Sankalp-G <sankalp.gawali@gmail.com>
2023-10-09 23:09:15 +0530

    add navbar

---
+++ app/NavMenu.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+export default function NavMenu() {
+  return (
+    <div className="navbar bg-base-300 shadow-lg">
+      <div className="flex-1">
+        <Link href={'/'} className="btn btn-ghost normal-case text-xl">MySpace</Link>
+      </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal gap-1 px-1 text-md tracking-wide">
+          <li><Link href={'/about'}>About</Link></li>
+          <li><Link href={'/blog'}>Blog</Link></li>
+          <li><Link href={'/users'}>Users</Link></li>
+        </ul>
+      </div>
+    </div>
+  )
+}
