1df4a5ab5e7f6472aa9c403908aaed7fec534477
Sankalp-G <sankalp.gawali@gmail.com>
2023-10-11 18:55:25 +0530

    add sign out and sign in buttons

--- app/NavMenu.tsx
+++ app/NavMenu.tsx
@@ -1,20 +1,34 @@
 import Link from "next/link";
 import ThemeToggle from "./ThemeToggle";
+import { SignInButton, SignOutButton } from "@/components/buttons";
 
 export default function NavMenu() {
   return (
     <div className="navbar bg-base-200 shadow-md">
       <div className="flex-1">
-        <Link href={'/'} className="btn btn-ghost normal-case text-xl">MySpace</Link>
+        <Link href={"/"} className="btn btn-ghost normal-case text-xl">
+          MySpace
+        </Link>
       </div>
       <div className="flex-none">
-        <ul className="menu menu-horizontal sm:gap-1 sm:px-1 text-md sm:tracking-wide">
-          <li><Link className="px-2 sm:px-3" href={'/about'}>About</Link></li>
-          <li><Link className="px-2 sm:px-3" href={'/blog'}>Blog</Link></li>
-          <li><Link className="px-2 sm:px-3" href={'/users'}>Users</Link></li>
+        <ul className="menu menu-horizontal align-center sm:gap-1 sm:px-1 text-md sm:tracking-wide">
+          <li>
+            <Link className="px-2 sm:px-3" href={"/about"}>About</Link>
+          </li>
+          <li>
+            <Link className="px-2 sm:px-3" href={"/blog"}>Blog</Link>
+          </li>
+          <li>
+            <Link className="px-2 sm:px-3" href={"/users"}>Users</Link>
+          </li>
+          <div className="divider divider-horizontal w-1 mx-0"></div>
+          <li className="justify-center flex-row">
+            <SignOutButton />
+            <SignInButton />
+          </li>
         </ul>
       </div>
       <ThemeToggle />
     </div>
-  )
+  );
 }
---
+++ components/buttons.tsx
@@ -0,0 +1,71 @@
+"use client"
+
+import { Session } from "next-auth";
+import { signIn, signOut, useSession } from "next-auth/react";
+import Image from "next/image";
+import Link from "next/link";
+
+
+function Avatar() {
+  const { data: session, status } = useSession();
+
+  if (typeof session?.user?.image == undefined) {
+    return (
+      <Link href={"/dashboard"} className="p-0">
+        <div className="avatar placeholder w-[32px] h-[32px]">
+          <div className="bg-neutral-focus text-neutral-content rounded-full w-[32px] h-[32px]">
+            <span className="text-3xl">K</span>
+          </div>
+        </div>
+      </Link>
+    );
+  }
+  else {
+    return (
+      <Link href={"/dashboard"} className="p-0">
+        <div className="avatar w-[32px] h-[32px]">
+          <Image
+            className="w-[32px] h-[32px] p-0 m-0 object-contain rounded-full text-center flex items-center bg-base-300"
+            src={session?.user?.image!}
+            width={32}
+            height={32}
+            alt="User"
+          />
+        </div>
+      </Link>
+    )
+  }
+}
+
+export function SignInButton() {
+  const { status } = useSession();
+
+  if (status == "loading") {
+    return <span className="loading loading-spinner loading-sm opacity-25 "></span>;
+  }
+
+  if (status == "authenticated") {
+    return (
+      <Avatar />
+    );
+  }
+
+  return (
+    <button
+      className="btn btn-sm btn-primary ml-3"
+      onClick={() => signIn()}
+    >Sign In</button>
+  )
+}
+
+export function SignOutButton() {
+  const { status } = useSession();
+
+  if (status == "authenticated") {
+    return (
+      <button onClick={() => signOut()}>Sign Out</button>
+    )
+  }
+
+  return <></>
+}
