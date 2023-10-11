78c1d667652df94ec10c0ad2e5e3a4f327b12ed2
Sankalp-G <sankalp.gawali@gmail.com>
2023-10-11 13:19:14 +0530

    add generateStaticParams to blog routes

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -10,11 +10,21 @@ interface Props {
   params: { slug: String }
 }
 
+export async function generateStaticParams() {
+  const posts: Post[] = await fetch("http://localhost:3000/api/content").then(
+    res => res.json()
+  )
+
+  return posts.map(
+    post => { slug: post.slug }
+  )
+}
+
 export default async function BlogPostPage({ params }: Props) {
   const posts: Post[] = await fetch("http://localhost:3000/api/content").then(
     res => res.json()
   )
-  const post = posts.find(post => post.slug = params.slug)!;
+  const post = posts.find(post => post.slug == params.slug)!;
 
   return (
     <>
