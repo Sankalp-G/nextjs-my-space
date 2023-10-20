994839cd7246cefd572e86b4b1aabad4639c1df0
Sankalp-G <sankalp.gawali@gmail.com>
2023-10-20 21:26:00 +0530

    add navbar mobile view/responsiveness

--- app/NavMenu.tsx
+++ app/NavMenu.tsx
@@ -4,31 +4,38 @@ import { SignInButton, SignOutButton } from "@/components/buttons";
 
 export default function NavMenu() {
   return (
-    <div className="navbar bg-base-200 shadow-md">
-      <div className="flex-1">
+    <div className="navbar flex-wrap sm:flex-nowrap bg-base-200 shadow-md">
+      <div className="flex-1 w-full sm:w-auto flex justify-between">
         <Link href={"/"} className="btn btn-ghost normal-case text-xl">
           MySpace
         </Link>
+
+        <div className="flex sm:hidden items-center justify-center mr-4">
+          <SignInButton />
+        </div>
       </div>
-      <div className="flex-none">
-        <ul className="menu menu-horizontal align-center sm:gap-1 sm:px-1 text-md sm:tracking-wide">
+      <div className="flex-none w-full sm:w-auto px-4 sm:px-0">
+        <ul className="px-0 menu menu-horizontal align-center gap-6 text-md sm:tracking-wide w-full sm:w-auto">
           <li>
-            <Link className="px-2 sm:px-3" href={"/about"}>About</Link>
+            <Link className="px-0" href={"/about"}>About</Link>
           </li>
           <li>
-            <Link className="px-2 sm:px-3" href={"/blog"}>Blog</Link>
+            <Link className="px-0" href={"/blog"}>Blog</Link>
           </li>
           <li>
-            <Link className="px-2 sm:px-3" href={"/users"}>Users</Link>
+            <Link className="px-0" href={"/users"}>Users</Link>
           </li>
-          <div className="divider divider-horizontal w-1 mx-0"></div>
-          <li className="items-center flex-row">
-            <SignOutButton />
+          <li className="items-center flex-row ml-auto sm:ml-0 hidden sm:flex">
             <SignInButton />
           </li>
+          <div className="flex items-center flex-row ml-auto sm:ml-0 sm:hidden">
+            <ThemeToggle />
+          </div>
         </ul>
+        <div className="hidden sm:flex items-center justify-center self-end sm:self-auto">
+          <ThemeToggle />
+        </div>
       </div>
-      <ThemeToggle />
     </div>
   );
 }
