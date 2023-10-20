5249b2690db3236eb4873abdbd673e870c5c4b08
Sankalp-G <sankalp.gawali@gmail.com>
2023-10-20 21:25:29 +0530

    add signout button to dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,15 +3,22 @@ import { authOptions } from "@/lib/auth";
 import { prisma } from "@/lib/prisma";
 import PageHeading from "@/components/PageHeading";
 import ProfileForm from "./ProfileForm";
+import { SignOutButton } from "@/components/buttons";
+import { redirect } from "next/navigation";
 
 export default async function Dashboard() {
   const session = await getServerSession(authOptions);
+  if (!session) {
+    redirect('/')
+  }
   const user = await prisma.user.findUnique({ where: { id: session?.user.id } })
 
   return (
     <>
-      <PageHeading title={"Update Profile"} />
+      <PageHeading title={"Sign Out"} />
+      <SignOutButton />
 
+      <PageHeading title={"Update Profile"} />
       <ProfileForm user={user} />
     </>
   )
