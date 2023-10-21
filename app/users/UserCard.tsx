375d5af5a0d7700068e9c4776fe2899bc4823f59
Sankalp-G <sankalp.gawali@gmail.com>
2023-10-21 19:22:17 +0530

    fix user image scaling

--- app/users/UserCard.tsx
+++ app/users/UserCard.tsx
@@ -9,7 +9,7 @@ interface Props {
 export default function UserCard({ id, name, image }: Props) {
   return (
     <div className="card w-64 h-96 bg-base-100 shadow-xl flex flex-col items-center">
-      <figure className="flex-1"><img className="flex-1" src={image} alt={name + "'s profile"} /></figure>
+      <figure className="flex-1 w-full"><img className="flex-1" src={image} alt={name + "'s profile"} /></figure>
       <div className="card-body flex-none w-full flex flex-col items-center gap-4">
         <h2 className="card-title">{name}</h2>
         {/* <p>If a dog chews shoes whose shoes does he choose?</p> */}
