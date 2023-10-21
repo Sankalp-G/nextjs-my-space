ee33daa2a1e83ff643980f2a9e21762353cd9a7c
Sankalp-G <sankalp.gawali@gmail.com>
2023-10-21 17:37:09 +0530

    fix nav button padding

--- app/NavMenu.tsx
+++ app/NavMenu.tsx
@@ -17,13 +17,13 @@ export default function NavMenu() {
       <div className="flex-none w-full sm:w-auto px-4 sm:px-0">
         <ul className="px-0 menu menu-horizontal align-center gap-6 text-md sm:tracking-wide w-full sm:w-auto">
           <li>
-            <Link className="px-0" href={"/about"}>About</Link>
+            <Link className="px-2 -mx-2 py-1 rounded-2x" href={"/about"}>About</Link>
           </li>
           <li>
-            <Link className="px-0" href={"/blog"}>Blog</Link>
+            <Link className="px-2 -mx-2 py-1 rounded-2x" href={"/blog"}>Blog</Link>
           </li>
           <li>
-            <Link className="px-0" href={"/users"}>Users</Link>
+            <Link className="px-2 -mx-2 py-1 rounded-2x" href={"/users"}>Users</Link>
           </li>
           <li className="items-center flex-row ml-auto sm:ml-0 hidden sm:flex">
             <SignInButton />
